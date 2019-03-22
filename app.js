7603a74d732785dc28abd5534c8a9d7d13435665
radhikamehta1997 <radhikamehta@cmail.carleton.ca>
2019-03-22 01:21:12 -0400

    whack a mole

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const LISTEN_PORT = 8080; //make sure greater than 3000. Some ports are reserved
 app.use((express.static(__dirname + '/public'))); //set root dir to the public folder
 
 //routes
-app.get('/index', function(req,res) {
-    res.sendFile(__dirname + '/public/index.html');
+app.get('/player1', function(req,res) {
+    res.sendFile(__dirname + '/public/player1.html');
 });
 
-/*app.get('/controller', function(req,res) {
-    res.sendFile(__dirname + '/public/controller.html');
-});*/
+app.get('/player2', function(req,res) {
+    res.sendFile(__dirname + '/public/player2.html');
+});
 
 //websocket stuff
 socketIO.on('connection', function(socket) {
